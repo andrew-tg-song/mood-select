99addbf1dfe5e3a06f7f69360bf7a3d4e740d2ea
Andrew Song <ontoro339@gmail.com>
2023-05-26 23:33:31 +0900

    chore: Add version up command

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'design']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'design', 'chore']],
     'type-case': [2, 'always', 'lower-case'],
     'subject-case': [2, 'always', ['sentence-case']],
   },
