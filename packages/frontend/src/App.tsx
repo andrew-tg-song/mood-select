cf1940f398eb40a6be8d612dc0829c68fd225d78
Yoo Seungmin <dnjfht@gmail.com>
2023-05-29 22:04:21 +0900

    design : NavBar component UI CSS

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,23 +1,34 @@
-import { useState } from 'react';
-import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './pages/Main';
 import NotFoundPage from './pages/NotFoundPage';
 import Detail from './pages/Detail';
 import Cart from './pages/Cart';
 import Root from './pages/Root';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import MyPage from './pages/MyPage';
+import Order from './pages/Order';
+import WishList from './pages/WishList';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    errorElement: <NotFoundPage />,
     children: [
       { index: true, element: <Main /> },
       { path: '/detail/:detailId', element: <Detail /> },
       { path: '/cart', element: <Cart /> },
+      { path: '/mypage', element: <MyPage /> },
+      { path: '/order', element: <Order /> },
+      { path: '/wish-list', element: <WishList /> },
+      { path: '*', element: <NotFoundPage /> },
     ],
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  { path: '/join', element: <Join /> },
 ]);
 
 function App() {
--- packages/frontend/src/components/NavBar.tsx
+++ packages/frontend/src/components/NavBar.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import ReactLogo from '../assets/logo.png';
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div className="w-[100%] h-[180px] bg-slate-100">
+      <div className="w-[100%] h-[120px]">
+        <div className="w-[93.75rem] h-[100%] mx-auto flex flex-row items-center justify-between relative">
+          <div className="w-[216px] h-[100%] flex items-center absolute left-[50%] ml-[-108px]">
+            <img src={ReactLogo} className="w-[100%] h-[80px] object-cover" />
+          </div>
+
+          <ul className="text-[#999999] text-[0.75rem] flex absolute right-0">
+            <li className="mr-[1.43rem]">로그인</li>
+            <li className="mr-[1.43rem]">회원가입</li>
+            <li className="mr-[1.43rem]">비회원 주문 조회</li>
+            <li className="mr-[1.43rem]">고객센터</li>
+            <li>공지</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
