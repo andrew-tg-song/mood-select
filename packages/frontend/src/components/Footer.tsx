699fb32fbe1bfe76cb905f546b009f504ffe2619
Yoo Seungmin <dnjfht@gmail.com>
2023-05-30 18:53:00 +0900

    design : Footer component DarkMode UI CSS

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -14,12 +14,12 @@ export default function Footer() {
           : 'bg-[#ebebeb] text-[#6b6b6b] border-t-[0.8px] border-[#ebebeb] border-solid'
       } w-[100%] text-[0.8125rem] font-sans`}
     >
-      <div className="w-[93.75rem] h-[100%] pt-[3rem] pb-[4.375rem] box-border mx-auto flex flex-col justify-between relative">
+      <div className="w-[93.75rem] h-[100%] py-[4.375rem] box-border mx-auto flex flex-col justify-between relative">
         <div className="w-[216px]">
           <img className="w-[100%] height-[100%] object-cover" src={`${darkMode === 'light' ? lightLogo : darkLogo}`} />
         </div>
 
-        <div className="w-[100%] h-[100%] mt-[1rem] flex flex-row">
+        <div className="w-[100%] h-[100%] mt-[1.875rem] flex flex-row">
           <div className="w-[50%] h-[100%]">
             <ul className="flex">
               <li className="pr-[1.2rem] border-r-[0.8px] border-[#9f9f9f]">브랜드 소개</li>
@@ -37,7 +37,9 @@ export default function Footer() {
               <br />
               address. 11111 부산광역시 금정구 금단로...
             </p>
-            <p>Copyright ⓒ 2023 All rights reserved. designed by SEUNGMIN.</p>
+            <p>
+              Copyright ⓒ 2023 All rights reserved. designed by <span className="font-bold">SEUNGMIN</span>.
+            </p>
           </div>
 
           <div className="w-[50%] h-[100%]">
@@ -45,7 +47,7 @@ export default function Footer() {
               <li>
                 <p className="mb-[2.125rem] font-bold">CS Center</p>
                 <p className="text-[1.125rem] font-bold mb-[1.25rem]">1111-1111</p>
-                <p>
+                <p className="text-[0.875rem] leading-[1.6rem]">
                   평일 AM 10:00 - PM 05:00
                   <br />
                   점심 PM 12:00 - PM 01:00
@@ -55,7 +57,7 @@ export default function Footer() {
               </li>
               <li>
                 <p className="mb-[2.125rem] font-bold">RETURNS.DELIVERY</p>
-                <p>
+                <p className="text-[0.875rem] leading-[1.6rem]">
                   CJ대한통운택배 (1588-1255)
                   <br />
                   부산광역시 부산진구 신암로 9,
@@ -65,7 +67,7 @@ export default function Footer() {
               </li>
               <li>
                 <p className="mb-[2.125rem] font-bold">BANK INFO</p>
-                <p>
+                <p className="text-[0.875rem] leading-[1.6rem]">
                   국민. 000000-00-000000
                   <br />
                   농협. 000-0000-0000-00
