093edf3896de1b00b64fa0b1ac22c864723309f1
Yoo Seungmin <dnjfht@gmail.com>
2023-05-30 23:08:43 +0900

    fix : scroll top 기능 수정

--- packages/frontend/src/components/QuickMenu.tsx
+++ packages/frontend/src/components/QuickMenu.tsx
@@ -6,14 +6,33 @@ import { AiOutlineWechat } from 'react-icons/ai';
 export default function QuickMenu() {
   const { darkMode } = useContext(DarkModeContext);
 
+  const [scrollY, setScrollY] = useState(0);
   const [showButton, setShowButton] = useState(false);
 
-  const scrollToTop = () => {
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+  };
+
+  const handleTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+    setScrollY(0);
+    setShowButton(false);
   };
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow);
+    };
+
+    watch();
+
+    return () => {
+      window.removeEventListener('scroll', handleFollow);
+    };
+  }, []);
 
   useEffect(() => {
     const handleShowButton = () => {
@@ -25,31 +44,33 @@ export default function QuickMenu() {
     return () => window.removeEventListener('scroll', handleShowButton);
   }, []);
 
+  console.log(scrollY);
+
   return (
     <div
       className={`${darkMode === 'light' ? 'text-white' : 'text-[#0f0f0f]'} ${
-        showButton ? 'block' : 'hidden'
-      } fixed right-[12px] bottom-[12px] text-[2rem]`}
+        showButton ? 'opacity-1' : 'opacity-0 transition ease-in-out'
+      } fixed right-[12px] bottom-[12px] text-[2rem] z-[99999]`}
     >
       <div
         className={`${
           darkMode === 'light' ? 'bg-[#0f0f0f]' : 'bg-white'
-        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] flex justify-center items-center cursor-pointer`}
+        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] flex justify-center items-center cursor-pointer animate-bounce`}
       >
         <AiOutlineWechat />
       </div>
       <div
-        onClick={scrollToTop}
+        onClick={handleTop}
         className={`${
           darkMode === 'light' ? 'bg-[#0f0f0f]' : 'bg-white'
-        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] mt-[10px] flex justify-center items-center cursor-pointer`}
+        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] mt-[10px] flex justify-center items-center cursor-pointer animate-bounce`}
       >
         <BsChevronUp />
       </div>
       <div
         className={`${
           darkMode === 'light' ? 'bg-[#0f0f0f]' : 'bg-white'
-        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] mt-[10px] flex justify-center items-center cursor-pointer`}
+        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] mt-[10px] flex justify-center items-center cursor-pointer animate-bounce`}
       >
         <BsChevronDown />
       </div>
