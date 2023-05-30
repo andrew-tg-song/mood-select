de04e56ef165331f3ac819664006e11e90509be5
Yoo Seungmin <dnjfht@gmail.com>
2023-05-30 20:06:59 +0900

    design : QuickMenu component UI CSS

--- packages/frontend/src/components/QuickMenu.tsx
+++ packages/frontend/src/components/QuickMenu.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DarkModeContext } from '../context/DarkModeContext';
+import { BsChevronUp, BsChevronDown } from 'react-icons/bs';
+import { AiOutlineWechat } from 'react-icons/ai';
 
 export default function QuickMenu() {
-  return <div>QuickMenu</div>;
+  const { darkMode } = useContext(DarkModeContext);
+
+  return (
+    <div
+      className={`${
+        darkMode === 'light' ? 'text-white' : 'text-[#0f0f0f]'
+      } fixed right-[12px] bottom-[12px] text-[2rem]`}
+    >
+      <div
+        className={`${
+          darkMode === 'light' ? 'bg-[#0f0f0f]' : 'bg-white'
+        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] flex justify-center items-center cursor-pointer`}
+      >
+        <AiOutlineWechat />
+      </div>
+      <div
+        className={`${
+          darkMode === 'light' ? 'bg-[#0f0f0f]' : 'bg-white'
+        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] mt-[10px] flex justify-center items-center cursor-pointer`}
+      >
+        <BsChevronUp />
+      </div>
+      <div
+        className={`${
+          darkMode === 'light' ? 'bg-[#0f0f0f]' : 'bg-white'
+        } w-[60px] h-[60px] rounded-full drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] mt-[10px] flex justify-center items-center cursor-pointer`}
+      >
+        <BsChevronDown />
+      </div>
+    </div>
+  );
 }
--- packages/frontend/src/components/Suggestion.tsx
+++ packages/frontend/src/components/Suggestion.tsx
@@ -9,7 +9,7 @@ export default function Suggestion() {
     <div
       className={`${
         darkMode === 'light' ? 'bg-black text-white' : 'bg-white text-black'
-      } w-[56px] h-[116px] p-[1rem]  box-border z-[99999] fixed right-0 top-[50%] mt-[-27.5px] rounded-l-2xl drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] [writing-mode:vertical-rl] flex items-center justify-between`}
+      } w-[56px] h-[116px] p-[1rem]  box-border z-[99999] fixed right-0 top-[50%] mt-[-58px] rounded-l-2xl drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] [writing-mode:vertical-rl] flex items-center justify-between`}
     >
       <BsFillEmojiSmileFill className="text-[1.4rem] rotate-90" />
       <p className="font-['Peddana'] text-[1.4rem] leading-4">
