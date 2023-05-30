5c400f6d4e30ba6b1ced55725698bf62f6e44942
Yoo Seungmin <dnjfht@gmail.com>
2023-05-30 19:41:44 +0900

    design : MOOD SELECT UI CSS

--- packages/frontend/src/components/Suggestion.tsx
+++ packages/frontend/src/components/Suggestion.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DarkModeContext } from '../context/DarkModeContext';
+import { BsFillEmojiSmileFill } from 'react-icons/bs';
 
 export default function Suggestion() {
-  return <div>Suggestion</div>;
+  const { darkMode } = useContext(DarkModeContext);
+
+  return (
+    <div
+      className={`${
+        darkMode === 'light' ? 'bg-black text-white' : 'bg-white text-black'
+      } w-[56px] h-[116px] p-[1rem]  box-border z-[99999] fixed right-0 top-[50%] mt-[-27.5px] rounded-l-2xl drop-shadow-[0_5px_2px_rgba(0,0,0,0.25)] [writing-mode:vertical-rl] flex items-center justify-between`}
+    >
+      <BsFillEmojiSmileFill className="text-[1.4rem] rotate-90" />
+      <p className="font-['Peddana'] text-[1.4rem] leading-4">
+        MOOD
+        <br />
+        SELECT
+      </p>
+    </div>
+  );
 }
--- packages/frontend/src/pages/Main.tsx
+++ packages/frontend/src/pages/Main.tsx
@@ -4,5 +4,9 @@ import { DarkModeContext } from '../context/DarkModeContext';
 export default function Main() {
   const { darkMode } = useContext(DarkModeContext);
 
-  return <div className={`${darkMode === 'light' ? 'bg-white text-black' : 'bg-[#0f0f0f] text-white'}`}>Main</div>;
+  return (
+    <div className={`${darkMode === 'light' ? 'bg-white text-black' : 'bg-[#0f0f0f] text-white'} h-[2000px]`}>
+      <div className="w-[81.25rem] mx-auto py-[8.75rem]">Main</div>
+    </div>
+  );
 }
